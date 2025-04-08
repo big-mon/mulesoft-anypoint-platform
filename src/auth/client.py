53f9b6e9081afaa311408777a0a61f303d7dc3e0
MON <mrmon93+github@gmail.com>
2025-04-08 17:07:17 +0900

    chore: 変数名

--- src/auth/client.py
+++ src/auth/client.py
@@ -10,30 +10,30 @@ class AuthClient:
 
     def __init__(self):
         load_dotenv()
-        self.client_id = os.getenv('ANYPOINT_CLIENT_ID')
-        self.client_secret = os.getenv('ANYPOINT_CLIENT_SECRET')
-        self.base_url = os.getenv('ANYPOINT_BASE_URL')
-        self.session = requests.Session()
-        self._access_token = None
+        self.__access_token = None
+        self.__client_id = os.getenv('ANYPOINT_CLIENT_ID')
+        self.__client_secret = os.getenv('ANYPOINT_CLIENT_SECRET')
+        self._base_url = os.getenv('ANYPOINT_BASE_URL')
+        self._session = requests.Session()
 
     def get_access_token(self):
         """Connected Appを使用してアクセストークンを取得"""
-        if self._access_token:
-            return self._access_token
+        if self.__access_token:
+            return self.__access_token
 
-        auth_url = f"{self.base_url}/accounts/api/v2/oauth2/token"
+        auth_url = f"{self._base_url}/accounts/api/v2/oauth2/token"
         payload = {
             'grant_type': 'client_credentials',
-            'client_id': self.client_id,
-            'client_secret': self.client_secret
+            'client_id': self.__client_id,
+            'client_secret': self.__client_secret
         }
         response = self.session.post(auth_url, data=payload)
         response.raise_for_status()
-        
-        self._access_token = response.json()['access_token']
-        return self._access_token
+
+        self.__access_token = response.json()['access_token']
+        return self.__access_token
 
     def refresh_token(self):
         """アクセストークンを強制的に更新"""
-        self._access_token = None
+        self.__access_token = None
         return self.get_access_token()
--- src/utils/config.py
+++ src/utils/config.py
@@ -5,22 +5,22 @@ from dotenv import load_dotenv
 
 class Config:
     """設定管理クラス"""
-    
+
     def __init__(self):
         load_dotenv()
-        self.client_id = os.getenv('ANYPOINT_CLIENT_ID')
-        self.client_secret = os.getenv('ANYPOINT_CLIENT_SECRET')
-        self.organization_id = os.getenv('ANYPOINT_ORGANIZATION_ID')
-        self.environment_id = os.getenv('ANYPOINT_ENVIRONMENT_ID')
-        self.base_url = os.getenv('ANYPOINT_BASE_URL', 'https://anypoint.mulesoft.com')
+        self.__client_id = os.getenv('ANYPOINT_CLIENT_ID')
+        self.__client_secret = os.getenv('ANYPOINT_CLIENT_SECRET')
+        self._organization_id = os.getenv('ANYPOINT_ORGANIZATION_ID')
+        self._environment_id = os.getenv('ANYPOINT_ENVIRONMENT_ID')
+        self._base_url = os.getenv('ANYPOINT_BASE_URL', 'https://anypoint.mulesoft.com')
 
     @property
     def is_valid(self):
         """必要な設定値が全て存在するか確認"""
         return all([
-            self.client_id,
-            self.client_secret,
-            self.organization_id,
-            self.environment_id,
-            self.base_url
+            self.__client_id,
+            self.__client_secret,
+            self._organization_id,
+            self._environment_id,
+            self._base_url
         ])
