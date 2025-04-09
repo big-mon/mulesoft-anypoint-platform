76f01f4f0a2d247a5517b7cfde5ee80ccb199652
MON <mrmon93+github@gmail.com>
2025-04-09 14:52:53 +0900

    feat: アプリケーション情報の取得

--- src/api/accounts.py
+++ src/api/accounts.py
@@ -11,19 +11,19 @@ class AccountsAPI:
     def __init__(self, token):
         load_dotenv()
         self._base_url = os.getenv('ANYPOINT_BASE_URL')
-        self._session = requests.Session()
-        self._session.headers.update({
+        self._organization_id = os.getenv('ANYPOINT_ORGANIZATION_ID')
+        self.__session = requests.Session()
+        self.__session.headers.update({
             'Authorization': f'Bearer {token}'
         })
-        self.__organization_id = os.getenv('ANYPOINT_ORGANIZATION_ID')
 
     def get_organization_environments(self):
         """組織内の環境の取得"""
-        url = f"{self._base_url}/accounts/api/organizations/{self.__organization_id}/environments"
+        url = f"{self._base_url}/accounts/api/organizations/{self._organization_id}/environments"
         payload = {
           'extended': False,
           'resolveTheme': False
         }
-        response = self._session.get(url, data=payload)
+        response = self.__session.get(url, data=payload)
         response.raise_for_status()
         return response.json()
--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 """Anypoint Platform API Client"""
 
+import json
+import os
 from auth.client import AuthClient
 from api.accounts import AccountsAPI
+from api.api_manager import APIManagerClient
 from utils.config import Config
 from utils.exceptions import ConfigurationError
+from datetime import datetime
 
 def main():
     """メイン処理"""
@@ -32,10 +36,33 @@ def main():
         environments = []
         print("組織情報の取得に成功しました：")
         for env in organization['data']:
-            environments.append({"name": env['name'], "id": env['id']})
+            environments.append({"name": env['name'], "env_id": env['id'], "org_id": env['organizationId']})
 
     except Exception as e:
         print(f"組織情報の取得時にエラーが発生しました: {e}")
+
+    try:
+        # API Managerクライアントの初期化
+        api_manager_client = APIManagerClient(token, environments)
+
+        # アプリケーションの取得
+        applications = api_manager_client.get_applications()
+        print("アプリケーションの取得に成功しました：")
+
+        # 出力先の準備
+        now = datetime.now()
+        timestamp_str = now.strftime("%Y%m%d_%H%M%S")
+        folder_path = os.path.join("output", timestamp_str)
+        os.makedirs(folder_path, exist_ok=True)
+
+        # 出力先へ出力
+        file_path = f'{folder_path}/applications.json'
+        with open(file_path, 'w', encoding='utf-8') as f:
+            json.dump(applications, f, indent=2, ensure_ascii=False)
+        print(f"アプリケーションの出力に成功しました：{file_path}")
+
+    except Exception as e:
+        print(f"アプリケーションの取得時にエラーが発生しました: {e}")
 
 if __name__ == "__main__":
     main()
